c4b673c2ce8f3141efca52462b52f05667986ac0
migo3825 <120005531+migo3825@users.noreply.github.com>
2023-04-13 14:57:56 +0800

    Update main.c

--- Lab#6/main.c
+++ Lab#6/main.c
@@ -2,23 +2,23 @@
 
 int multi(int i, int j)
 {
-    if (i > 9)
+    if (i<=9)
     {
-        return 0;
-    }
-    else
-    {
-        if (j > 9)
+        if (j<=9)
+        {
+            printf("%d*%d=%-2d ", i, j, i*j);
+            multi(i, j+1);
+        }
+        else
         {
             printf("\n");
             multi(i+1, 1);
             return 0;
         }
-        else
-        {
-        printf("%d*%d=%-2d ", i, j, i*j);
-        multi(i, j+1);
-        }
+    }
+    else
+    {
+        return 0;
     }
 }
 
